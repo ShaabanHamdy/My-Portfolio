132a543e7b2feb2cda88efcc7e9b5ce178f9f58e
Shaaban <shabanhamdy94@gmail.com>
2023-07-27 20:27:45 +0300

    first commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img from "../images/IMG-20200915-WA0010-01.jpeg";
+import img from "../images/IMG_20210827_131147_021.jpeg";
 import cv from "../images/shaaban Resume Web Developer.pdf";
 import "./about.css";
 import { FaCloudDownloadAlt } from "react-icons/fa";
@@ -13,20 +13,22 @@ const About = () => {
         <div className="row">
           <div className="col-md-12 imageText">
             <div className="">
-              <img className="img" src={img} alt="" />
+              <img className="img"  src={img} alt="" />
             </div>
             <div className="aboutMe">
+              <div className="">
               <p>
-                " I'm Shaaban Hamdy Shaaban 26 years old ,
+                " I'm Shaaban Hamdy Shaaban 26 years old,
                 <br /> I live in Giza and I have a Bachelor's degree in Business
                 Administration
                 <br /> and I'm also a MERN Stack Developer Certified from Route
                 IT Training Center "
               </p>
-              <div className="m-5">
+              </div>
+              <div className="Download">
                 <a target="_blank" href={cv}>
-                  <button className="btn btn-info Download">
-                    Download My CV <FaCloudDownloadAlt size={30} />
+                  <button className="btn btn-info Download-btn">
+                    Download My CV <FaCloudDownloadAlt className="ms-2" size={30} />
                   </button>
                 </a>
               </div>
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,78 +4,75 @@ import "./home.css";
 
 const Home = () => {
   return (
-
     <>
-      
       <div className="container-fluid ">
         <div className="row">
-         <div className="col-md-12 both">
-         <div className="images"></div>
-          <div className="content ">
-            <h1 className="fw-bolder">
-              !Hello, I'm <span className="text-warning"> sHaaBan HaMdy</span>
-            </h1>
-            <h3 className="text-center lh-lg">
-              <Typed
-                className=""
-                loop
-                strings={[
-                  `I'm a MERN Stack developer`,
-                  "Using  MongoDB",
-                  "Using Express",
-                  "Using React js",
-                  "Using Node js",
-                ]}
-                typeSpeed={150}
-              />
-            </h3>
-            <div className="divIcons">
-              <a
-                className=" text-white"
-                target="_blanket"
-                href="https://www.facebook.com/shabanhamdy24/"
-              >
-                <i className="icon fab fa-facebook"></i>
-              </a>
-              <a
-                className=" text-white"
-                target="_blanket"
-                href="https://github.com/ShaabanHamdy"
-              >
-                <i className="icon fab fa-github"></i>
-              </a>
-              <a
-                className=" text-white"
-                target="_blanket"
-                href="https://www.linkedin.com/in/shaban-hamdy/"
-              >
-                <i className="icon fab fa-linkedin"></i>
-              </a>
-              <a
-                className=" text-white"
-                target="_blanket"
-                href="https://www.instagram.com/shaaban.97/"
-              >
-                <i className="icon fab fa-instagram"></i>
-              </a>
-              <a
-                className=" text-white"
-                target="_blanket"
-                href="https://twitter.com/SHIiKO1397"
-              >
-                <i className="icon fab fa-twitter"></i>
-              </a>
+          <div className="col-md-12 both">
+            <div className="images"></div>
+            <div className="content ">
+
+              <div className=" text-center">
+              <h1 className="fw-bolder">
+                <span className="Hello-p" >!Hello, </span> I'm{" "}
+                <span className="text-warning shaban-hamdy"> sHaaBan HaMdy</span>
+              </h1>
+              </div>
+              <h3 className="text-center lh-lg">
+                <Typed
+                  className=""
+                  loop
+                  strings={[
+                    `I'm a MERN Stack developer`,
+                    "Using  MongoDB",
+                    "Using Express",
+                    "Using React js",
+                    "Using Node js",
+                  ]}
+                  typeSpeed={150}
+                />
+              </h3>
+              <div className="divIcons">
+                <a
+                  className=" text-white"
+                  target="_blanket"
+                  href="https://www.facebook.com/shabanhamdy24/"
+                >
+                  <i className="icon fab fa-facebook"></i>
+                </a>
+                <a
+                  className=" text-white"
+                  target="_blanket"
+                  href="https://github.com/ShaabanHamdy"
+                >
+                  <i className="icon fab fa-github"></i>
+                </a>
+                <a
+                  className=" text-white"
+                  target="_blanket"
+                  href="https://www.linkedin.com/in/shaban-hamdy/"
+                >
+                  <i className="icon fab fa-linkedin"></i>
+                </a>
+                <a
+                  className=" text-white"
+                  target="_blanket"
+                  href="https://www.instagram.com/shaaban.97/"
+                >
+                  <i className="icon fab fa-instagram"></i>
+                </a>
+                <a
+                  className=" text-white"
+                  target="_blanket"
+                  href="https://twitter.com/SHIiKO1397"
+                >
+                  <i className="icon fab fa-twitter"></i>
+                </a>
+              </div>
             </div>
           </div>
-         </div>
         </div>
       </div>
-   
-   
-       
-      
     </>
-
   );
 };
 
