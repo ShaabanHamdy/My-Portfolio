7c23651416474e2bb3bca312068830c914e22ae0
Shaaban <shabanhamdy94@gmail.com>
2024-08-07 11:58:33 +0300

    first commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import img from "../images/IMG_20210827_131147_021.jpeg";
-import cv from "../images/shaaban Resume Web Developer.pdf";
+import cv from "../images/Shaaban-Hamdy-Resume.pdf";
 import style from "./about.module.css";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 const About = () => {
@@ -12,15 +11,14 @@ const About = () => {
         </div>
         <div className="row">
           <div className={`col-md-12 ${style.imageText}`}>
-            <div>{/* <img className={style.img}  src={img} alt="" /> */}</div>
-
             <div className={style.aboutMe}>
               <div>
                 <p>
-                  " My name is Shaaban Hamdy Shaaban, I'm 26 years old,
+                  " My Name's Shaaban Hamdy Shaaban, I'm 27 years old,
                   <br /> I live in Giza and I have a bachelor's degree in
                   business administration
-                  <br /> and I'm also a MERN Stack Developer Certified from
+                  <br /> 
+                  and I'm also a MERN Stack Developer Certified from
                   Route IT Training Center "
                 </p>
               </div>
